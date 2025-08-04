b5de5b4e3815dc32ecb048d5b50b5d50e9f08925
Dogtuna Dev <mrjonnydavis@gmail.com>
2025-08-04 18:27:15 -0500

    feat: add cookie consent and privacy updates

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { ProjectProvider } from "./context/ProjectContext.jsx";
 import PropTypes from "prop-types";
+import { initAnalytics, getAnalyticsConsent } from "./utils/analytics.js";
 
 // Ensure PropTypes is available globally for any components expecting it
 window.PropTypes = PropTypes;
+
+const consent = getAnalyticsConsent();
+if (consent === 'granted') {
+  initAnalytics();
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
